1cb44bf8a7b8d0f5db2f05125e8290173d5db253
Oleksandr <zoleksandr06@gmail.com>
2026-03-28 13:46:46 +0200

    fix docker

---
+++ Program.cs
@@ -0,0 +1,109 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebAPI_2.Abstract;
+using WebAPI_2.Core;
+using WebAPI_2.DAL;
+using WebAPI_2.DAL.Abstracts;
+using WebAPI_2.DAL.Repositories;
+using WebAPI_2.Services;
+
+namespace WebAPI_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            // Add CORS services
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:3000", "http://localhost:3001")
+                              .AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowCredentials();
+                    });
+            });
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            //builder.Services.AddOpenApi();
+
+            builder.Services.AddSingleton<UsersStore>();
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen();
+
+            var jwt = builder.Configuration.GetSection("Jwt");
+            var key = jwt["Key"];
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwt["Issuer"],
+                        ValidAudience = jwt["Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(key))
+                    };
+                });
+
+            builder.Services.AddAuthorization();
+
+            
+
+            //for DB
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Register repositories
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+            builder.Services.AddScoped<IBookRepository, BookRepository>();
+            builder.Services.AddScoped<IGenreRepository, GenreRepository>();
+
+            //Register Services
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IAuthorService, AuthorService>();
+            builder.Services.AddScoped<IBookService, BookService>();
+            builder.Services.AddScoped<IGenreService, GenreService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                //app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            // IMPORTANT: Add CORS before Authorization
+            app.UseCors(MyAllowSpecificOrigins);
+
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
